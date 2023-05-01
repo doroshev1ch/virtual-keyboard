17ef90785fb4ba44f55bd12e8c1e35aefa51d3bf
doroshev1ch <doroshevich.2002@mail.ru>
2023-05-01 15:46:10 +0300

    feat: add functionality to 'Shift','Enter','Backspace' and other modifier keys

--- virtual-keyboard/index.js
+++ virtual-keyboard/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-restricted-syntax */
 // basic keyboard/input layout
-const inputConteiner = document.createElement('input');
+const textConteiner = document.createElement('textarea');
 const keyboardContainer = document.createElement('div');
 keyboardContainer.classList.add('keyboard-container');
-inputConteiner.classList.add('input-conteiner');
+textConteiner.classList.add('text-conteiner');
 
 const rows = [
   ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
-  ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\', 'Del'],
+  ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\\'],
   ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', "'", 'Enter'],
   ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '\u2191', 'Shift'],
   ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '\u2190', '\u2193', '\u2192', 'Ctrl'],
@@ -27,21 +27,52 @@ for (const row of rows) {
   keyboardContainer.appendChild(rowContainer);
 }
 
-document.body.appendChild(inputConteiner);
+document.body.appendChild(textConteiner);
 document.body.appendChild(keyboardContainer);
 
-// Space properties
+// modifier keys properties
 const spaceElement = document.querySelector('.keyboard-row:nth-child(5) .keyboard-key:nth-child(4)');
 spaceElement.style.width = '310px';
 
 const keyboardKeys = document.querySelectorAll('.keyboard-key');
+let isCapsLockOn = false;
+let isShiftPressed = false;
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Shift') {
+    isShiftPressed = true;
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  if (event.key === 'Shift') {
+    isShiftPressed = false;
+  }
+});
+
 keyboardKeys.forEach((key) => {
   key.addEventListener('click', function print() {
     const keyText = this.textContent;
     if (keyText === 'Space') {
-      inputConteiner.value += ' ';
+      textConteiner.value += ' ';
+    } else if (keyText === 'CapsLock') {
+      isCapsLockOn = !isCapsLockOn;
+      this.classList.toggle('active');
+    } else if (keyText === 'Shift') {
+      isCapsLockOn = !isCapsLockOn;
+      this.classList.toggle('active');
+    } else if (keyText === 'Backspace') {
+      textConteiner.value = textConteiner.value.slice(0, -1);
+    } else if (keyText === 'Tab') {
+      textConteiner.value += '\t';
+    } else if (keyText === 'Enter') {
+      textConteiner.value += '\n';
+    } else if (keyText === 'Alt') {
+      textConteiner.value += '\n';
     } else {
-      inputConteiner.value += keyText.toLowerCase();
+      // eslint-disable-next-line no-bitwise
+      const letter = isCapsLockOn ^ isShiftPressed ? keyText.toUpperCase() : keyText.toLowerCase();
+      textConteiner.value += letter;
     }
   });
 });
